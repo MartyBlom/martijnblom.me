33878dd2198ec095790e4a479f928c01dd288056
MartyBlom <hello@martijnblom.me>
2024-12-18 17:41:14 +0100

    Did a lot

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,8 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Cabin', 'sans-serif'], // Add Cabin as the default sans-serif font
-      },
-    },
+    extend: {}
   },
 
-  plugins: [],
+  plugins: []
 } satisfies Config;
